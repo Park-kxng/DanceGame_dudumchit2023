6519b54160eccecb2e92d97b0dd94b5c52a2c603
Onb209 <so209@naver.com>
2023-06-04 02:33:42 +0900

    포즈비교하는 Server_addposesim.py 추가

--- 3D_Pose_Estimation_Code/withUnity/Server_addposesim.py
+++ 3D_Pose_Estimation_Code/withUnity/Server_addposesim.py
@@ -4,6 +4,23 @@ import cv2
 import mediapipe as mp
 import numpy as np
 
+# 생성된 춤 미디어파이프 결과 npy 파일 불러옴
+generated_dance = np.load('./dance_mediapipe.npy')
+print("생성된 춤 shape: ", generated_dance.shape)
+rows,columns = generated_dance.shape
+frames = int(rows/33)
+flag_frame = 0
+
+# 포즈 비교 함수 - (x,y)는 유클리드, z는 맨하탄 거리 비교
+def pose_similarity_2d_depth(dance, user_motion, alpha=0.5, beta=0.5):
+    i = flag_frame
+    x1, y1, d1 = dance[33*i:33*(i+1), 0], dance[33*i:33*(i+1), 1], dance[33*i:33*(i+1), 2]
+    x2, y2, d2 = user_motion[:, 0], user_motion[:, 1], user_motion[:, 2]
+    location_dist = np.sqrt((x1-x2)**2 + (y1-y2)**2)
+    depth_dist = np.abs(d1-d2)
+    similarity = np.exp(-alpha * location_dist - beta * depth_dist)
+    return similarity
+
 # 서버 주소와 포트번호
 HOST = '127.0.0.1'
 PORT = 9999
@@ -54,6 +71,7 @@ while True:
         # Extract landmarks
         try:
             landmarks = results.pose_landmarks.landmark
+
             #landmarks = np.array([[landmark.x, landmark.y, landmark.z, landmark.visibility] for landmark in landmarks])
             #####print(landmarks)
             print("=======================")
@@ -65,16 +83,35 @@ while True:
             #print(data)
 
             #########
+
+            # 생성된 춤과 사용자의 춤 비교하여 정확도 계산
+            print("flag:", flag_frame)
+            user_motion = np.array([[landmark.x, landmark.y, landmark.z] for landmark in landmarks])
+            print("user_motion: ",user_motion.shape)
+            posesimilarity = pose_similarity_2d_depth(generated_dance, user_motion)
+            print("posesimilarity: ", posesimilarity)
+            # 정확도가 낮은 조인트 파란색으로 변경하기 위함
+            ####### 우선 정확도 0.5 기준으로 해둠. 나중에 변경
+            posesimilarity_result = np.where(posesimilarity < 0.5, 0, 1) # 정확도가 0.5보다 작으면 0, 크면 1
+            print("posesimilarity: ", posesimilarity_result)
+
             landmarks_str = ""
+            i=0
             for landmark in landmarks:
-                landmarks_str += str(landmark.x) + "," + str(landmark.y) + "," + str(landmark.z) +",";
+                landmarks_str += str(landmark.x) + "," + str(landmark.y) + "," + str(landmark.z) +"," + str(posesimilarity_result[i]) +"," ;
+                i+=1
 
             client_socket.send(landmarks_str.encode())
             print(landmarks_str)
-            print(landmarks_str.encode())
-            landmarks_str = ""
-            print(landmarks_str)
+            flag_frame += 1
+            #print(landmarks_str.encode())
+            #landmarks_str = ""
+            #print(landmarks_str)
+
+            # 생성된 춤 종료 후 처리 필요
+
         except:
+            print("pass")
             pass
 
         # Show image
